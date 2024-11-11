2ad08e59b1cc531f2fe527b61d017cba7280e9e5
MindokRuvaak <cole_daniel@rocketmail.com>
2024-11-11 13:54:12 +0100

    Added a test for set constructor

--- lab1/src/test/java/tdbv/lab1/SetTest.java
+++ lab1/src/test/java/tdbv/lab1/SetTest.java
@@ -1,10 +1,22 @@
 package tdbv.lab1;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
 import org.junit.Test;
 
+
 public class SetTest {
-    
-    
+    Set testset;
+
+    @Before
+    public void setup() {
+        testset = new Set();
+    }
+
+
+    @Test
+    public void constructorTest(){
+        assertTrue(testset instanceof Set);
+    }
     
 }
