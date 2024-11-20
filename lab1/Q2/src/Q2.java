a66064ed08bc0fcbb8525aae538b68a73ff6a6a0
MindokRuvaak <cole_daniel@rocketmail.com>
2024-11-20 12:25:19 +0100

    started on report file, as txt for now

--- lab1/Q2/src/Q2.java
+++ lab1/Q2/src/Q2.java
@@ -1,7 +1,11 @@
 
-
 public class Q2 {
     public static void main(String[] args) {
         WorkSchedule ws = new WorkSchedule(5);
+
+        ws.setRequiredNumber(0, 0, 0);
+
+        ws.nextIncomplete(0);
+
     }
 }
