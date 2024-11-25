c4c498ad41e52483eb46133eb7375fe5578ce40d
MindokRuvaak <cole_daniel@rocketmail.com>
2024-11-25 16:13:42 +0100

    forgot some files

---
+++ lab1/ex2/Set.java
@@ -0,0 +1,90 @@
+package tdbv.lab1;
+
+import java.util.ArrayList;
+import java.util.function.IntBinaryOperator;
+
+public class Set {
+  private final ArrayList<Integer> a;
+
+  public Set() {
+    a = new ArrayList<>();
+  }
+
+  public int[] toArray() {
+    int[] ia = new int[a.size()];
+    for (int i = 0; i < ia.length; i++) {
+      ia[i] = a.get(i);
+    }
+    return ia;
+  }
+
+  public void insert(int x) {
+    for (int i = 0; i < a.size(); i++) {
+      if (a.get(i) > x) {
+        a.add(i, x);
+        return;
+      } else {
+        if (a.get(i) == x) {
+          return; //bugfix: replaced break with return
+        }
+      }
+    }
+    a.add(x);
+  }
+
+  public boolean member(int x) {
+    for (int i = 0; i < a.size(); i++) {
+      if (a.get(i) > x) {
+        return false;
+      } else {
+        if (a.get(i) == x) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  public Set intersect(Set s) {
+    Set res = new Set(); 
+    ArrayList<Integer> b = s.a;
+    for(int i = 0, j = 0 ; i < a.size() && j < b.size();) {
+      if (a.get(i).equals(b.get(j))){
+        res.insert(a.get(i));
+        i++;
+        j++;
+      } else {
+        if (a.get(i) < b.get(j)) {
+          //a.remove(i);
+          i++;
+        } else {
+          j++;
+        }
+      }
+    }
+    return res;
+  }
+
+  // Try with:
+  //   (a, b) -> a + b;
+  //   (a, b) -> a - b;
+  public boolean distinctClosed(IntBinaryOperator f) {
+    int vi,vj;
+    for (int i = 0; i < a.size(); i++) {
+      for (int j = i; j < a.size(); j++) {
+        vi = a.get(i);
+        vj = a.get(j);
+        if (
+          !(
+            member(f.applyAsInt(vi, vj)
+            ) 
+          || 
+          vi == vj)
+          ) {
+            return false;
+          }
+      }
+    }
+    return true;
+  }
+}
---
+++ lab1/ex2/SetTest.java
@@ -0,0 +1,105 @@
+package tdbv.lab1;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class SetTest {
+    Set testSet;
+
+    @Before
+    public void setup() {
+        testSet = new Set();
+        testSet.insert(10);
+    }
+
+
+    @Test
+    public void constructorTest(){
+        assertTrue(testSet instanceof Set);
+    }
+
+    @Test
+    public void insertTest(){
+        Set s = new Set();
+
+        assertTrue(s.toArray().length == 0);
+        
+        s.insert(10);
+
+        assertTrue(s.toArray().length == 1);
+        assertTrue(s.toArray()[0] == 10);
+
+        s.insert(5);
+
+        assertTrue(s.toArray().length == 2);
+        assertTrue(s.toArray()[0] == 5);
+        assertTrue(s.toArray()[1] == 10);
+
+        
+        s.insert(10);
+        assertTrue(s.toArray().length == 2);
+        assertTrue(s.toArray()[0] == 5);
+        assertTrue(s.toArray()[1] == 10);
+
+    }
+
+    @Test
+    public void memberTest(){
+        assertTrue(testSet.member(10));
+        assertFalse(testSet.member(5));
+        assertFalse(testSet.member(15));
+    }
+
+    @Test
+    public void intersectTestSC(){
+        Set a = new Set();
+        a.insert(10);
+        a.insert(15);
+        Set b = new Set();
+        b.insert(5);
+        b.insert(20);
+        b.insert(10);
+
+        Set res = a.intersect(b);
+        assertTrue(res.member(10));
+        assertTrue(res.toArray().length == 1);
+        assertFalse(res.member(15));
+        assertFalse(res.member(5));
+        assertFalse(res.member(20));
+    }
+
+    @Test
+    public void intersectTestBC(){
+        Set a = new Set();
+        a.insert(10);
+        a.insert(15);
+
+        Set b = new Set();
+        b.insert(10);
+        
+        Set res = a.intersect(b);
+        
+        assertTrue(res.member(10));
+        assertTrue(res.toArray().length == 1);
+        assertFalse(res.member(15));
+        assertFalse(res.member(5));
+
+    }
+
+    @Test
+    public void distinctClosedTest(){
+        Set s  = new Set();
+        s.insert(0);
+        s.insert(1);
+        s.insert(2);
+        assertFalse(s.distinctClosed( (a,b)-> a+b ));
+        assertFalse(s.distinctClosed( (a,b)-> a-b ));
+        
+        assertTrue(testSet.distinctClosed( (a,b)-> a+b ));
+        assertTrue(testSet.distinctClosed( (a,b)-> a-b ));
+    }
+    
+}
